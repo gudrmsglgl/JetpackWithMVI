40d030fa5648a2cbd34211de833852b01a48602d
davidHG <gudrmsglgl@gmail.com>
2019-12-04 02:37:16 +0900

    setup navigation launchFrag code

--- app/src/main/java/com/fastival/jetpackwithmviapp/ui/auth/LauncherFragment.kt
+++ app/src/main/java/com/fastival/jetpackwithmviapp/ui/auth/LauncherFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.fastival.jetpackwithmviapp.R
+import kotlinx.android.synthetic.main.fragment_launcher.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +22,36 @@ class LauncherFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_launcher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        register.setOnClickListener {
+            navRegisteration()
+        }
+
+        login.setOnClickListener {
+            navLogin()
+        }
+
+        forgot_password.setOnClickListener {
+            navForgotPassword()
+        }
+
+        focusable_view.requestFocus()
+    }
+
+    private fun navForgotPassword() {
+        findNavController().navigate(R.id.action_launcherFragment_to_forgotPasswordFragment)
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
+    }
+
+    private fun navRegisteration() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
     }
 
 
